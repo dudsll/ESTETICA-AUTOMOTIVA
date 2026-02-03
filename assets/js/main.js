8875fd0ef878237e3531fd5591ea901ad417c1b1
Ëduardo <dudu_llopes@hotmail.com>
2026-02-02 23:09:30 -0300

    pesquisando funcao de trocar a imagem. ja adicionei os botoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 <script>
 
+    const imagem = document.getElementById('minhaImagem');
+    const botao= document.querySelectorAll('altImg');
 
+    botao.addEventListener('click', function() {
+       if (imagem.src.match("foto1.jpg")) {
+            imagem.src = "foto2.jpg";
+        } else {
+            imagem.src = "foto1.jpg";
+        }
+    });
 
-    
 </script>
