32683956bf6cac849bc4e384d262419122ef0a69
Ëduardo <dudu_llopes@hotmail.com>
2026-02-10 23:18:33 -0300

    adicionando diversas melhorias

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,19 @@ botoes.forEach(botao => {
     imagem.src = botao.dataset.img;
   });
 });
+
+const imgs = document.querySelectorAll('.slides img');
+const prox = document.querySelector('#prox');
+const ant = document.querySelector('#ant');
+let i = 0;
+
+function changeSlide(n) {
+  imgs[i].classList.remove('active');
+  i = (i + n + imgs.length) % imgs.length;
+  imgs[i].classList.add('active');
+}
+
+prox.addEventListener('click', () => changeSlide(1));
+ant.addEventListener('click', () => changeSlide(-1));
+
+setInterval(() => changeSlide(1), 5000);
