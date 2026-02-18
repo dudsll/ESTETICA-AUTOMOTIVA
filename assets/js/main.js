4e6508e5619a0d274b7080f7f77544c0a8278324
Ëduardo <dudu_llopes@hotmail.com>
2026-02-17 22:13:15 -0300

    adicionando overlay e scroll lock no documento

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,11 @@ setInterval(() => changeSlide(1), 5000);
 
 const menu = document.getElementById("menu");
 const botao = document.getElementById("menu-button");
+const body = document.body;
+const overlay = document.getElementById("overlay");
 
 botao.addEventListener("click", () => {
   menu.classList.toggle("ativo");
+  body.classList.toggle("no-scroll");
+  overlay.classList.toggle("active");
 });
