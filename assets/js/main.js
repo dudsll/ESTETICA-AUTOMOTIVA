1093171adeb008f90a5f706aa83c22ea752ade43
dudsll <dudu_llopes@hotmail.com>
2026-02-20 16:15:54 -0300

    arrumando a ordem em que as funcoes sao chamadas para que pare de quebrar o menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+const menu = document.getElementById("menu");
+const botao = document.getElementById("menu-button");
+const body = document.body;
+const overlay = document.getElementById("overlay");
+
+botao.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+  body.classList.toggle("no-scroll");
+  overlay.classList.toggle("active");
+}); 
+
 const imagem = document.getElementById("imagemPrincipal");
 const botoes = document.querySelectorAll("button.buttonC");
 
@@ -22,14 +33,3 @@ prox.addEventListener('click', () => changeSlide(1));
 ant.addEventListener('click', () => changeSlide(-1));
 
 setInterval(() => changeSlide(1), 5000);
-
-const menu = document.getElementById("menu");
-const botao = document.getElementById("menu-button");
-const body = document.body;
-const overlay = document.getElementById("overlay");
-
-botao.addEventListener("click", () => {
-  menu.classList.toggle("ativo");
-  body.classList.toggle("no-scroll");
-  overlay.classList.toggle("active");
-}); 
