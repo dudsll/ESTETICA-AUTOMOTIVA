d80256f9f69672324bfd069079aa9de064f3b887
Ëduardo <dudu_llopes@hotmail.com>
2026-02-21 23:29:14 -0300

    arrumando os arquivos JS

---
+++ assets/js/serviços.js
@@ -0,0 +1,8 @@
+const imagem = document.getElementById("imagemPrincipal");
+const botoes = document.querySelectorAll("button.buttonC");
+
+botoes.forEach(botao => {
+  botao.addEventListener("click", () => {
+    imagem.src = botao.dataset.img;
+  });
+});
