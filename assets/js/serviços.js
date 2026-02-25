e7ce8357af7729730669d612e2cbaf6990d14ebb
Ëduardo <dudu_llopes@hotmail.com>
2026-02-24 23:43:06 -0300

    resolve: conflito em servicos.js

---
+++ assets/js/serviços.js
@@ -0,0 +1,13 @@
+<<<<<<< Updated upstream
+=======
+export function initServicos() {
+  const imagem = document.getElementById("imagemPrincipal");
+  const botoes = document.querySelectorAll("button.buttonC");
+
+  botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+      imagem.src = botao.dataset.img;
+    });
+  }); 
+}
+>>>>>>> Stashed changes
