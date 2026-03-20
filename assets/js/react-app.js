2128297f3d14faeaebb498051518660be76cb1e8
dudsll <dudu__llopes@hotmail.com>
2026-03-20 15:37:15 -0300

    removendo tudo e comecando novamente

--- assets/js/react-app.js
+++
@@ -1,7 +0,0 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function App() {
-  return React.createElement("h1", null, "Site de Estética Automotiva");
-}
-
-root.render(React.createElement(App));
