8ade376d83b3caa9ad045f474d283678fce0c360
Ëduardo <dudu_llopes@hotmail.com>
2026-03-21 23:57:28 -0300

    criacao de novos componentes

---
+++ src/components/headerPrincipal.jsx
@@ -0,0 +1,26 @@
+function Header() {
+    return (
+        <header id="headerPrincipal">
+            <div className="menu-container">
+                <button id="menu-button">&#9776;</button>
+            </div>
+            <nav id="menu" className="menu">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/portfolio">Portfolio</a></li>
+                    <li><a href="/contact">Contato</a></li>
+                </ul>
+            </nav>
+            <div id="overlay"></div>
+            <div>
+                <div className="logo">
+                    <a href="/">
+                        <img src="assets/imgs/eduardo_garage_logo.svg" alt="Eduardo Garage logo - automotive detailing and customization services" />
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
